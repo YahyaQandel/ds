c97e74626abe63192b1e1b71a4115708c6b67fbd
YahyaQandel <yqandel@cloudinn.net>
2021-12-28 02:31:41 +0200

    fix mid position

--- bs.go
+++ bs.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func binarySearh(arr []int, value int) int {
 	l, r := 0, len(arr)-1
 	for l <= r {
@@ -13,17 +9,12 @@ func binarySearh(arr []int, value int) int {
 		} else if value > arr[mid] {
 			l = mid + 1
 		} else {
-			r = mid
+			r = mid - 1
 		}
 	}
 	return -1
 }
 
 func main() {
-	fmt.Println(binarySearh([]int{10, 20, 30, 40, 50}, 10))
-	fmt.Println(binarySearh([]int{10, 20, 30, 40, 50}, 20))
-	fmt.Println(binarySearh([]int{10, 20, 30, 40, 50}, 30))
-	fmt.Println(binarySearh([]int{10, 20, 30, 40, 50}, 40))
-	fmt.Println(binarySearh([]int{10, 20, 30, 40, 50}, 50))
-	fmt.Println(binarySearh([]int{10, 20, 30, 40, 50}, 60))
+
 }
